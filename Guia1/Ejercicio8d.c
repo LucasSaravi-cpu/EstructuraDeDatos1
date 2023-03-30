eba1c1f309f30f7aac13a038342dc65d23f0d2a2
LucasSaravi-cpu <126580800+LucasSaravi-cpu@users.noreply.github.com>
2023-03-30 16:14:43 -0300

    Update Ejercicio8d.c

--- Guia1/Ejercicio8d.c
+++ Guia1/Ejercicio8d.c
@@ -4,8 +4,8 @@ void main(){
 
 char caracteres[100];
 printf("Ingrese los caracteres que quieras, para terminar apreta enter \n");
-gets(caracteres);
-puts(caracteres);
+gets(caracteres); // lee la cadena de caracteres
+puts(caracteres); // imprime la cadena de caracteres
 printf("La cantidad de caracteres son: %d",strlen(caracteres));
 
 }
