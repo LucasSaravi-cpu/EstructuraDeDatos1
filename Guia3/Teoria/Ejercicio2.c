cc6a1b53c8dc6bd181cacae773e6858580ccaf1f
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2023-05-05 11:37:10 -0300

    Add files via upload

---
+++ Guia3/Teoria/Ejercicio2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n, resultado;
+
+    printf("Ingrese el valor de n: ");
+    scanf("%d", &n);
+
+    resultado = fibonacci(n);
+
+    printf("El termino %d de la sucesion de Fibonacci es %d", n, resultado);
+
+    return 0;
+}
