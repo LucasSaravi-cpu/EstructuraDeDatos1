49e1612830894f0c6ff7b67871671c320636181b
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2026-04-11 00:20:09 -0300

    Add files via upload

---
+++ Clase 2 Recursividad/Video1 Obtener el minimo de un vector.c
@@ -0,0 +1,30 @@
+//Obtener el minimo de un vector de forma recursiva
+#include <stdio.h>
+
+
+
+int minimo(int v[], int n);
+
+int main() {
+    int v[] = {5, 2, 8, -3, 7};
+    int n =int minimo(int v[], int n) 5;
+
+    printf("Minimo: %d", minimo(v, n - 1));
+
+    return 0;
+}
+
+ {
+    int min;
+
+    if (n == 0)
+        return v[0];
+    else {
+        min = minimo(v, n - 1);
+
+        if (v[n] < min)
+            return v[n];
+        else
+            return min;
+    }
+}
