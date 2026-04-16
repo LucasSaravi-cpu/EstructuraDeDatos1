b9f33d2cdb736e783ddf803f94b36c3870459ede
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2026-04-15 23:14:22 -0300

    Update EjercicioD.c

--- Clase 2 Recursividad/EjercicioD.c
+++ Clase 2 Recursividad/EjercicioD.c
@@ -3,18 +3,19 @@
 #define MAX 10
 
 void cargarMatrizInt(int A[][10], int n, int m);
-int sumaM(int A[][10], int i, int j, int n, int m);
+//int sumaM(int A[][10], int i, int j, int n, int m);
 int esVocal(char c) ;
-void vocalesPorColumna(char A[][MAX], int i, int j, int n, int m, int cont[]);
-int filaTieneVocal(char A[][MAX], int i, int j, int n);
-void cargarMatrizChar(char A[][MAX], int n, int m);
+void cantVocalC(char A[][MAX], int i, int j, int n, int cant);
+int cantvocalF(char A[][MAX], int i, int j, int n);
+void cargarMatrizChar(char A[][MAX], int n);
+int sumaM(int A[][10], int i, int j, int n);
+int todasFilasConVocal(char A[][MAX], int i, int j, int n, int encontro) ;
 int cantColumnasPares(int A[][MAX], int i, int j, int n);
 
-int main()
-{
+int main(){
 
     int n, m;
-    int A[10][10];
+    int A[MAX][MAX];
      char B[MAX][MAX];
 
    printf("Filas (n): ");
@@ -24,26 +25,27 @@ int main()
 
    cargarMatrizInt(A,n,m);
 
+  // printf ("Ejercicio 0 \n");
+ //  printf ("La suma de elementos de A: %d", sumaM(A,0,0,n-1,m-1));
+
     printf("Ejercicio 1 \n");
-    printf("Suma total: %d\n", sumaM(A,n-1,m-1,n-1,m-1));
+    printf("Suma total: %d\n", sumaM(A,n-1,m-1,n-1));
 
    printf("Ejercicio 2 \n");
-    int cont[MAX] = {0};
-    cargarMatrizChar(B, n, m);
 
-    vocalesPorColumna(B, 0, 0, n, m, cont);
+    cargarMatrizChar(B, n);
 
-    printf("\nVocales por columna:\n");
-    for (int j = 0; j < m; j++) {
-        printf("Columna %d: %d\n", j, cont[j]);
-    }
+    cantVocalC(B, 0, 0, n-1, 0);
+
 
     printf("Ejercicio 3 \n");
-    printf("\nFilas con al menos una vocal: %d\n", cantvocf(B, 0, n));
+    printf("\nFilas con al menos una vocal: %d\n", cantvocalF(B, 0,0, n-1));
+
+
 
     printf("Ejercicio 4 \n");
-    if (todasFilasConVocal(B, 0, n))
-        printf("Todas las filas tienen vocal\n");
+    if (todasFilasConVocal(B, 0, 0,n,0))
+        printf("Todas las filas tienen almenos una vocal\n");
     else
         printf("NO todas las filas tienen vocal\n");
 
@@ -61,20 +63,49 @@ void cargarMatrizInt(int A[][10], int n, int m){
             scanf("%d",&A[i][j]);
         }
 }
+/*
+//Ejercicio 0
+//Obtener la suma de una matriz nxm elementos
+//Recorre la matriz de izquierda a derecha y de arriba hacia abajo
+//la suma se va armando cuando vuelve (de atrás hacia adelante)
+int sumaM(int A[][10], int i, int j, int n, int m){
+  //Caso base Cuando llega a la última posición de la matriz, devuelve ese valor y termina el programa
+    if(i == n && j == m)
+        return A[i][j];
+
+
+    //Si está en la última columna de una fila, hace:
+
+//Suma el valor actual A[i][j]
+//Baja a la siguiente fila (i+1)
+//Reinicia la columna en 0
+    if(j == m)
+        return A[i][j] + sumaM(A, i+1, 0, n, m);
+    else
+   // Sigue avanzando hacia la derecha en la misma fila.
+        return A[i][j] + sumaM(A, i, j+1, n, m);
+}*/
+
+
 
 
 //Ejercicio 1
-int sumaM(int A[][10], int i, int j, int n, int m){
+//Modificar sumaM() de modo ue la invocacion sea
+//printf("%d",SumaM(A,n-1,m-1,n-1));
+
+int sumaM(int A[][10], int i, int j, int n){
     if(i == 0 && j == 0)
         return A[i][j];
 
-    if(j == 0)
-        return A[i][j] + sumaM(A, i-1, m, n, m);
+    if(i == 0)
+        return A[i][j] + sumaM(A, n, j-1, n);
     else
-        return A[i][j] + sumaM(A, i, j-1, n, m);
+        return A[i][j] + sumaM(A, i-1, j, n);
 }
 
 //Ejercicio 2
+
+//Dado una matriz de NxN de caracteres desarrollar una funcion oid que muestre la cantidad de vocales por columnas
 
 
 int esVocal(char c) {
@@ -82,68 +113,90 @@ int esVocal(char c) {
             c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
 }
 
-void vocalesPorColumna(char A[][MAX], int i, int j, int n, int m, int cont[]) {
-    if (i == n)
-        return;
+void cantVocalC(char A[][MAX], int i, int j, int n ,int cant) {
 
-    if (j == m) {
-        vocalesPorColumna(A, i+1, 0, n, m, cont);
-        return;
+    if (j <= n) {  // mientras haya columnas
+
+        if (i <= n) {  // recorremos de arriba hacia abajo
+
+            //  cuenta en la IDA
+            if (esVocal(A[i][j])) {
+                cant++;
+            }
+
+            // Luego baja de fila y sigue en la misma columna y se le pasa si conto o no y se repite el ciclo
+            cantVocalC(A, i+1, j, n, cant);
+
+        } else {
+            // terminamos la columna
+            printf("Columna %d: %d\n", j, cant);
+
+            // pasamos a la siguiente columna y reiniciamos la fila
+            cantVocalC(A, 0, j+1, n, 0);
+        }
     }
-
-    if (esVocal(A[i][j])) {
-        cont[j]++;
-    }
-
-    vocalesPorColumna(A, i, j+1, n, m, cont);
 }
 
-
-
-//Ejercicio 3
-
-int filaTieneVocal(char A[][MAX], int i, int j, int n) {
-    if (j == n)
-        return 0;
-
-    if (esVocal(A[i][j]))
-        return 1;
-
-    return filaTieneVocal(A, i, j+1, n);
-}
-
-int cantvocf(char A[][MAX], int i, int n) {
-    if (i == n)
-        return 0;
-
-    return filaTieneVocal(A, i, 0, n) + cantvocf(A, i+1, n);
-}
-
-
-//Ejercicio 4
-
-int todasFilasConVocal(char A[][MAX], int i, int n) {
-    if (i == n)
-        return 1;
-
-    if (!filaTieneVocal(A, i, 0, n))
-        return 0;
-
-    return todasFilasConVocal(A, i+1, n);
-
-
-}
-
-
-void cargarMatrizChar(char A[][MAX], int n, int m) {
+void cargarMatrizChar(char A[][MAX], int n) {
     printf("Ingrese matriz de caracteres:\n");
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+        for (int j = 0; j < n; j++) {
             printf("A[%d][%d]: ", i, j);
             scanf(" %c", &A[i][j]);
         }
     }
 }
+
+//Ejercicio 3
+
+//Cuenta la cantidad de filas que almenos tengan una vocal
+
+//Ida: modificás variables antes de llamar  cant++
+//Vuelta: usás el return para construir el resultado
+
+int cantvocalF(char A[][MAX], int i, int j, int n) {
+ if ( i>n)
+    return 0;
+else
+    if (j>n)
+      return cantvocalF(A,i+1,0,n);
+    else
+       if (esVocal(A[i][j]))
+           return  cantvocalF(A,i+1,0,n)+1;
+
+        else
+           return cantvocalF(A,i,j+1,n);
+
+}
+
+
+
+//Ejercicio 4
+//Dada una matriz de nxn caracteres , desarrollar una funcion revursiva int que determine si todas las finales tinen alguna vocal
+int todasFilasConVocal(char A[][MAX], int i, int j, int n, int encontro) {
+
+    // Terminamos todas las filas
+    if (i == n)
+        return 1;
+
+    // Recorremos columnas de la fila i
+    if (j < n) {
+
+        if (esVocal(A[i][j]))
+            encontro = 1;
+
+        return todasFilasConVocal(A, i, j+1, n, encontro);
+    }
+
+    // Terminamos la fila i
+    if (encontro == 0)
+        return 0;  // esta fila NO tiene vocal
+
+    // Pasamos a la siguiente fila
+    return todasFilasConVocal(A, i+1, 0, n, 0);
+}
+
+
 
 
 int cantColumnasPares(int A[][MAX], int i, int j, int n) {
@@ -165,3 +218,4 @@ int cantColumnasPares(int A[][MAX], int i, int j, int n) {
     // Si llegó al final de la columna (todos eran pares)
     return 1 + cantColumnasPares(A, 0, j + 1, n);
 }
+
