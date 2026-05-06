79d9177b3b26725be2ea2e94244e249073433580
lucas saravi <126580800+LucasSaravi-cpu@users.noreply.github.com>
2026-05-06 11:42:24 -0300

    Update Pilas.h

--- Clase 4 pilas y colas/Ejercicio6/Pilas.h
+++ Clase 4 pilas y colas/Ejercicio6/Pilas.h
@@ -13,7 +13,7 @@ typedef nodop *TPila;
 
 void poneP(TPila *P, ElementoP x);
 void sacaP(TPila *P, ElementoP *x);
-
+ElementoP* consultaP(TPila P);
 int VaciaP(TPila P);
 void IniciaP(TPila *P);
 #endif // PILASDINAMICAS_H_INCLUDED
